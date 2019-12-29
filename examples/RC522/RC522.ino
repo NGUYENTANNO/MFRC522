3443a8e42057f27bb429372f2f3b47e4df661cac
NGUYENTANNO <44743864+NGUYENTANNO@users.noreply.github.com>
2019-12-29 22:23:59 +0700

    Update RC522.ino

--- examples/RC522/RC522.ino
+++ examples/RC522/RC522.ino
@@ -13,22 +13,16 @@ void setup()
 void loop() 
 {
     byte key[6] = {255,255,255,255,255,255};
+  //Đoạn chương trình sau dùng để ghi thẻ sử dụng lệnh nfc.ghi_the(4,key,data) 
+  // Trong đó 4: là block cần ghi, key là password, data là dữ liệu cần ghi vào block 4
  /* byte data[16] ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
    if(nfc.ghi_the(4,key,data))
     Serial.print("Success");
   else
     Serial.println("Fail");
- /* if(doc_the(9,key,data))
-    Serial.print("Success");
-  else
-    Serial.println("Fail");
-  delay(1000);
-  Serial.println();
-  for(byte i=0; i<=15;i++)
-  {
-    Serial.print(data[i]);Serial.print(",");
-  }
-  Serial.println();*/
+*/
+  //Đoạn chương trình sau dùng để đọc thẻ sử dụng lệnh nfc.ghi_the(4,key,data) 
+  // Trong đó 4: là block cần đọc, key là password, result là dữ liệu sau khi đọc xong được lưu vào đó
   byte  result[16];
   if(nfc.doc_the(4,key,result))
     Serial.println("Success");
@@ -37,7 +31,8 @@ void loop()
    
   for(byte i=0;i<16;i++)
   {
-   Serial.print(result[i]);Serial.print(",");
+    Serial.print(result[i]);
+    Serial.print(",");
   }
   Serial.println();
   delay(1000);
